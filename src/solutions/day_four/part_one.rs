1e459a9400c0788d62ceb51e81c91f6901abe0f0
JM <jm@tristar.ai>
2024-12-08 01:04:47 -0500

    optimize day 4 part 1 to the moon

--- src/solutions/day_four/part_one.rs
+++ src/solutions/day_four/part_one.rs
@@ -1,93 +1,91 @@
+#[derive(Debug)]
+pub struct U32 {
+  inner: [u8; 4],
+  _alignment: [u32; 0],
+}
+
+impl U32 {
+  #[inline(always)]
+  pub fn new() -> Self {
+    Self {
+      inner: [0; 4],
+      _alignment: [],
+    }
+  }
+}
+
+impl Index<usize> for U32 {
+  type Output = u8;
+  #[inline(always)]
+  fn index<'ref_life>(&'ref_life self, idx: usize) -> &'ref_life u8 {
+    &self.inner[idx]
+  }
+}
+
+impl IndexMut<usize> for U32 {
+  #[inline(always)]
+  fn index_mut<'ref_life>(&'ref_life mut self, idx: usize) -> &'ref_life mut u8 {
+    &mut self.inner[idx]
+  }
+}
+use std::ops::{Index, IndexMut};
+
 use super::{XMASSES, XMAS_FOR, XMAS_REV};
+// WAS 2378
 
 pub fn solution() -> u16 {
   let mut sum = 0;
+  let mut acomp = U32::new();
+
+  let mut xr0 = &XMASSES[0];
+  let mut xr1 = &XMASSES[0];
+  let mut xr2 = &XMASSES[0];
+  let mut xr3 = &XMASSES[0];
 
   for row in 0..140 {
     for col in 0..140 {
-      match (row, col) {
-        (0..137, 3..137) => {
-          let rrow = [
-            XMASSES[row][col],
-            XMASSES[row][col + 1],
-            XMASSES[row][col + 2],
-            XMASSES[row][col + 3],
-          ];
-          let rz = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col + 1],
-            XMASSES[row + 2][col + 2],
-            XMASSES[row + 3][col + 3],
-          ];
-          let dcol = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col],
-            XMASSES[row + 2][col],
-            XMASSES[row + 3][col],
-          ];
-          let lz = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col - 1],
-            XMASSES[row + 2][col - 2],
-            XMASSES[row + 3][col - 3],
-          ];
-          sum += (rrow == XMAS_FOR || rrow == XMAS_REV) as u16;
-          sum += (rz == XMAS_FOR || rz == XMAS_REV) as u16;
-          sum += (dcol == XMAS_FOR || dcol == XMAS_REV) as u16;
-          sum += (lz == XMAS_FOR || lz == XMAS_REV) as u16;
-        },
-        (0..137, 0..3) => {
-          let rrow = [
-            XMASSES[row][col],
-            XMASSES[row][col + 1],
-            XMASSES[row][col + 2],
-            XMASSES[row][col + 3],
-          ];
-          let rz = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col + 1],
-            XMASSES[row + 2][col + 2],
-            XMASSES[row + 3][col + 3],
-          ];
-          let dcol = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col],
-            XMASSES[row + 2][col],
-            XMASSES[row + 3][col],
-          ];
-          sum += (rrow == XMAS_FOR || rrow == XMAS_REV) as u16;
-          sum += (rz == XMAS_FOR || rz == XMAS_REV) as u16;
-          sum += (dcol == XMAS_FOR || dcol == XMAS_REV) as u16;
-        },
-        (0..137, 137..140) => {
-          let dcol = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col],
-            XMASSES[row + 2][col],
-            XMASSES[row + 3][col],
-          ];
-          let lz = [
-            XMASSES[row][col],
-            XMASSES[row + 1][col - 1],
-            XMASSES[row + 2][col - 2],
-            XMASSES[row + 3][col - 3],
-          ];
-          sum += (dcol == XMAS_FOR || dcol == XMAS_REV) as u16;
-          sum += (lz == XMAS_FOR || lz == XMAS_REV) as u16;
-        },
-        (137..140, 0..137) => {
-          let rrow = [
-            XMASSES[row][col],
-            XMASSES[row][col + 1],
-            XMASSES[row][col + 2],
-            XMASSES[row][col + 3],
-          ];
-          sum += (rrow == XMAS_FOR || rrow == XMAS_REV) as u16;
-        },
-        _ => {},
+      xr0 = &XMASSES[row];
+      acomp[0] = xr0[col];
+
+      if row < 137 {
+        xr1 = &XMASSES[row + 1];
+        xr2 = &XMASSES[row + 2];
+        xr3 = &XMASSES[row + 3];
+      }
+
+      if col < 137 {
+        acomp[1] = xr0[col + 1];
+        acomp[2] = xr0[col + 2];
+        acomp[3] = xr0[col + 3];
+        sum += unsafe { comp_tpun(&acomp) };
+      }
+      if row < 137 {
+        acomp[1] = xr1[col];
+        acomp[2] = xr2[col];
+        acomp[3] = xr3[col];
+        sum += unsafe { comp_tpun(&acomp) };
+      }
+      if row < 137 && col < 137 {
+        acomp[1] = xr1[col + 1];
+        acomp[2] = xr2[col + 2];
+        acomp[3] = xr3[col + 3];
+        sum += unsafe { comp_tpun(&acomp) };
+      }
+      if row < 137 && col > 2 {
+        acomp[1] = xr1[col - 1];
+        acomp[2] = xr2[col - 2];
+        acomp[3] = xr3[col - 3];
+        sum += unsafe { comp_tpun(&acomp) };
       }
     }
   }
 
   sum
+}
+
+#[inline(always)]
+unsafe fn comp_tpun(val: &U32) -> u16 {
+  let punned = *(val as *const _ as *const u32);
+
+  (punned == XMAS_FOR || punned == XMAS_REV) as u16
 }
