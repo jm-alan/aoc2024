710ef310414926c11668233436768ce83b4301ac
JM <jm@tristar.ai>
2024-12-13 20:32:20 -0500

    d31 < 0.5ms

--- src/solutions/day_three/part_one.rs
+++ src/solutions/day_three/part_one.rs
@@ -1,41 +1,69 @@
 use super::CORRUPTED_MEMORY;
 
-pub fn solution() -> u128 {
+pub fn solution() -> u32 {
   let mut state: u8 = 0;
   let mut sum = 0;
-  let mut lbuf = "".to_string();
-  let mut rbuf = "".to_string();
+  let mut left: [u32; 3] = [0; 3];
+  let mut right: [u32; 3] = [0; 3];
+  let mut lpos = 0;
+  let mut rpos = 0;
 
-  for ch in CORRUPTED_MEMORY.chars() {
+  for ch in CORRUPTED_MEMORY.bytes() {
     match (ch, state) {
-      ('m', 0) => state = 1,
-      ('u', 1) => state = 2,
-      ('l', 2) => state = 3,
-      ('(', 3) => state = 4,
-      ('0'..='9', 4) => {
+      (b'm', 0) => state = 1,
+      (b'u', 1) => state = 2,
+      (b'l', 2) => state = 3,
+      (b'(', 3) => state = 4,
+      (b'0'..=b'9', 4) => {
         state = 5;
-        lbuf.push(ch);
+        left[lpos] = (ch - 48) as u32;
+        lpos += 1;
       },
-      ('0'..='9', 5) => lbuf.push(ch),
-      (',', 5) => state = 6,
-      ('0'..='9', 6) => {
+      (b'0'..=b'9', 5) => {
+        left[lpos] = (ch - 48) as u32;
+        lpos += 1;
+      },
+      (b',', 5) => state = 6,
+      ((b'0'..=b'9'), 6) => {
         state = 7;
-        rbuf.push(ch);
+        right[rpos] = (ch - 48) as u32;
+        rpos += 1;
       },
-      ('0'..='9', 7) => rbuf.push(ch),
-      (')', 7) => {
+      (b'0'..=b'9', 7) => {
+        right[rpos] = (ch - 48) as u32;
+        rpos += 1;
+      },
+      (b')', 7) => {
         state = 0;
-        sum += lbuf.parse::<u128>().unwrap() * rbuf.parse::<u128>().unwrap();
-        lbuf = "".to_string();
-        rbuf = "".to_string();
+        digimul(&mut sum, &left[0..lpos], &right[0..rpos]);
+        lpos = 0;
+        rpos = 0;
       },
       _ => {
         state = 0;
-        lbuf = "".to_string();
-        rbuf = "".to_string();
+        lpos = 0;
+        rpos = 0;
       },
     }
   }
 
   sum
+}
+
+#[inline(always)]
+fn digimul(sum: &mut u32, left: &[u32], right: &[u32]) {
+  *sum += binconv(left) * binconv(right);
+}
+
+#[inline(always)]
+fn binconv(digital: &[u32]) -> u32 {
+  let mut val = 0u32;
+  *digital
+    .iter()
+    .rev()
+    .enumerate()
+    .fold(&mut val, |sum, (idx, val)| {
+      *sum += (*val) * 10u32.pow(idx as u32);
+      sum
+    })
 }
