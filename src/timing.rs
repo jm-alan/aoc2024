ce7c49881b0efc6c0174ffae17c69f3edf3b7849
JM <jm@tristar.ai>
2024-12-15 18:11:30 -0500

    it's okay to consume durations on every call to stopwatch because it's written over on every call as well

--- src/timing.rs
+++ src/timing.rs
@@ -6,12 +6,10 @@ static mut DURATIONS: [Duration; ITERATIONS] = [Duration::new(0, 0); ITERATIONS]
 static mut AVERAGES: [Duration; ITERATIONS] = [Duration::new(0, 0); ITERATIONS];
 static mut RESULTS: [i64; ITERATIONS] = [0; ITERATIONS];
 static mut LOCAL_DURATION: Duration = Duration::new(0, 0);
-static mut LOCAL_THEN: Option<Instant> = None;
-static mut LOCAL_START: Option<Instant> = None;
 static mut NEXT_AVERAGE: usize = 0;
 
 unsafe fn store() {
-  AVERAGES[NEXT_AVERAGE] = DURATIONS.iter().sum::<Duration>() / (ITERATIONS as u32);
+  AVERAGES[NEXT_AVERAGE] = DURATIONS.into_iter().sum::<Duration>() / (ITERATIONS as u32);
 }
 
 pub unsafe fn stopwatch(some_fn: fn() -> i64, title: &str) {
